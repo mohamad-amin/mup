0f36e638ed4806c990dc83a5e23e90da5213e9d3
mohamad-amin <mohammadi.mohamadamin@gmail.com>
2022-08-29 15:25:57 -0700

    MLP also uses config now

--- examples/MLP/main.py
+++ examples/MLP/main.py
@@ -1,5 +1,6 @@
 import time
 import os
+import shutil
 import pandas as pd
 import numpy as np
 import torch.nn.functional as F
@@ -13,16 +14,19 @@ import math
 from mup.coord_check import get_coord_data, plot_coord_data
 from mup import MuSGD, get_shapes, set_base_shapes, make_base_shapes, MuReadout
 
-def coord_check(mup, lr, train_loader, nsteps, nseeds, args, plotdir='', legend=False):
+from examples import example_utils
+
+
+def coord_check(mup, lr, train_loader, nsteps, nseeds, output_mult, input_mult, load_base_shapes, growth_factor, plotdir='', legend=False):
 
     def gen(w, standparam=False):
         def f():
-            model = MLP(width=w, nonlin=torch.tanh, output_mult=args.output_mult, input_mult=args.input_mult).to(device)
+            model = MLP(width=w, nonlin=torch.tanh, output_mult=output_mult, input_mult=input_mult, growth_factor=growth_factor).to(device)
             if standparam:
                 set_base_shapes(model, None)
             else:
-                assert args.load_base_shapes, 'load_base_shapes needs to be nonempty'
-                set_base_shapes(model, args.load_base_shapes)
+                assert load_base_shapes, 'load_base_shapes needs to be nonempty'
+                set_base_shapes(model, load_base_shapes)
             return model
         return f
 
@@ -62,90 +66,94 @@ if __name__ == '__main__':
 
     We provide below some optimal hyperparameters for different activation/loss function combos:
         if nonlin == torch.relu and criterion == F.cross_entropy:
-            args.input_mult = 0.00390625
-            args.output_mult = 32
+            input_mult = 0.00390625
+            output_mult = 32
         elif nonlin == torch.tanh and criterion == F.cross_entropy:
-            args.input_mult = 0.125
-            args.output_mult = 32
+            input_mult = 0.125
+            output_mult = 32
         elif nonlin == torch.relu and criterion == MSE_label:
-            args.input_mult = 0.03125
-            args.output_mult = 32
+            input_mult = 0.03125
+            output_mult = 32
         elif nonlin == torch.tanh and criterion == MSE_label:
-            args.input_mult = 8
-            args.output_mult = 0.125
+            input_mult = 8
+            output_mult = 0.125
     ''', formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('--save_base_shapes', type=str, default='',
-                        help='file location to save base shapes at')
-    parser.add_argument('--load_base_shapes', type=str, default='',
-                        help='file location to load base shapes from')
-    parser.add_argument('--seed', type=int, default=1)
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--epochs', type=int, default=20)
-    parser.add_argument('--momentum', type=float, default=0.9)
-    parser.add_argument('--lr', type=float, default=0.1)
-    parser.add_argument('--output_mult', type=float, default=1.0)
-    parser.add_argument('--input_mult', type=float, default=1.0)
-    parser.add_argument('--init_std', type=float, default=1.0)
-    parser.add_argument('--no_shuffle', action='store_true')
-    parser.add_argument('--log_interval', type=int, default=300)
-    parser.add_argument('--log_dir', type=str, default='.')
-    parser.add_argument('--data_dir', type=str, default='/tmp')
-    parser.add_argument('--train_size', type=int, default=-1)
-    parser.add_argument('--coord_check', action='store_true',
-                        help='test μ parametrization is correctly implemented by collecting statistics on coordinate distributions for a few steps of training.')
-    parser.add_argument('--coord_check_nsteps', type=int, default=3,
-                        help='Do coord check with this many steps.')
-    parser.add_argument('--coord_check_nseeds', type=int, default=5,
-                        help='number of seeds for testing correctness of μ parametrization')
-    parser.add_argument('--run_index', type=int, default=0)
 
+    parser.add_argument('--config_path', default='', help="Path to a config file")
+    parser.add_argument('--data_dir', type=str, default='/tmp')
+    parser.add_argument('--save_dir', default='', help='Path to dir to save checkpoints and logs')
     args = parser.parse_args()
 
+    os.makedirs(args.save_dir, exist_ok=True)
+    shutil.copyfile(args.config_path, os.path.join(args.save_dir, "config.yml"))
+    config = example_utils.load_config(args.config_path)
+
+    width_mult = config['model'].get('width_mult', 1)
+    growth_factor = config['model'].get('growth_factor', 2)
+    save_base_shapes = config['model'].get('save_base_shapes', '')
+    load_base_shapes = config['model'].get('load_base_shapes', '')
+    do_coord_check = config['model'].get('coord_check', False)
+    coord_check_nsteps = config['model']['coord_check_nsteps']
+    coord_check_nseeds = config['model']['coord_check_nseeds']
+    output_mult = config['model'].get('output_mult', 1.0)
+    input_mult = config['model'].get('input_mult', 1.0)
+    init_std = config['model'].get('init_std', 1.0)
+
+    lr = config['train']['lr']
+    epochs = config['train']['epochs']
+    batch_size = config['train']['batch_size']
+    num_workers = config['train']['num_workers']
+    momentum = config['train']['momentum']
+    criterion_name = config['train'].get('criterion', 'l2')
+    no_shuffle = config['train'].get('no_shuffle', True)
+
+    train_size = config['data']['train_size']
+    seed = config['data']['seed']
+    log_interval = config['data'].get('log_interval', 300)
+
     torch.manual_seed(args.seed)
-
     device = torch.device("cuda")
-
-    kwargs = {'num_workers': 1, 'pin_memory': True}
+    kwargs = {'num_workers': 0, 'pin_memory': True}
 
     transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     trainset = datasets.CIFAR10(root=args.data_dir, train=True, download=True, transform=transform)
-    if args.train_size != -1:
+    if train_size != -1:
         perm = torch.randperm(len(trainset))
-        idx = perm[:args.train_size]
+        idx = perm[:train_size]
         trainset = torch.utils.data.Subset(trainset, idx)
 
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
-                                            shuffle=not args.no_shuffle, num_workers=2, )
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=not no_shuffle, num_workers=0, )
 
     testset = datasets.CIFAR10(root=args.data_dir, train=False,
                                         download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size,
-                                            shuffle=False, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=0)
 
     classes = ('plane', 'car', 'bird', 'cat',
             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
     class MLP(nn.Module):
-        def __init__(self, width=128, num_classes=10, nonlin=F.relu, output_mult=1.0, input_mult=1.0):
+        def __init__(self, width=128, num_classes=10, nonlin=F.relu, output_mult=1.0, input_mult=1.0, growth_factor=1):
             super(MLP, self).__init__()
             self.nonlin = nonlin
             self.input_mult = input_mult
             self.output_mult = output_mult
             self.fc_1 = nn.Linear(3072, width, bias=False)
-            self.fc_2 = nn.Linear(width, width, bias=False)
-            self.fc_3 = MuReadout(width, num_classes, bias=False, output_mult=args.output_mult)
+            self.fc_2 = nn.Linear(width, int(width * growth_factor), bias=False)
+            self.fc_3 = MuReadout(int(width * growth_factor), num_classes, bias=False, output_mult=output_mult)
             self.reset_parameters()
 
         def reset_parameters(self):
             nn.init.kaiming_normal_(self.fc_1.weight, a=1, mode='fan_in')
             self.fc_1.weight.data /= self.input_mult**0.5
-            self.fc_1.weight.data *= args.init_std
+            self.fc_1.weight.data *= init_std
             nn.init.kaiming_normal_(self.fc_2.weight, a=1, mode='fan_in')
-            self.fc_2.weight.data *= args.init_std
+            self.fc_2.weight.data *= init_std
             nn.init.zeros_(self.fc_3.weight)
 
         def forward(self, x):
@@ -171,12 +179,12 @@ if __name__ == '__main__':
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
             optimizer.step()
-            if batch_idx % args.log_interval == 0:
+            if batch_idx % log_interval == 0:
                 elapsed = time.time() - start_time
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f} | ms/batch {:5.2f}'.format(
                     epoch, batch_idx * len(data), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader), loss.item(),
-                    elapsed * 1000 / args.log_interval))
+                    elapsed * 1000 / log_interval))
                 start_time = time.time()
             if scheduler is not None:
                 scheduler.step()
@@ -215,47 +223,50 @@ if __name__ == '__main__':
         y_onehot -= 1/10
         return F.mse_loss(output, y_onehot)
     
-    if args.coord_check:
+    if do_coord_check:
         print('testing parametrization')
         import os
         os.makedirs('coord_checks', exist_ok=True)
         plotdir = 'coord_checks'
-        coord_check(mup=True, lr=args.lr, train_loader=train_loader, nsteps=args.coord_check_nsteps, nseeds=args.coord_check_nseeds, args=args, plotdir=plotdir, legend=False)
-        coord_check(mup=False, lr=args.lr, train_loader=train_loader, nsteps=args.coord_check_nsteps, nseeds=args.coord_check_nseeds, args=args, plotdir=plotdir, legend=False)
+        coord_check(mup=True, lr=lr, train_loader=train_loader, nsteps=coord_check_nsteps, nseeds=coord_check_nseeds, output_mult=output_mult, input_mult=input_mult, load_base_shapes=load_base_shapes, growth_factor=growth_factor, plotdir=plotdir, legend=False)
+        coord_check(mup=False, lr=lr, train_loader=train_loader, nsteps=coord_check_nsteps, nseeds=coord_check_nseeds, output_mult=output_mult, input_mult=input_mult, load_base_shapes=load_base_shapes, growth_factor=growth_factor, plotdir=plotdir, legend=False)
         import sys; sys.exit()
 
     logs = []
     # for nonlin in [torch.relu, torch.tanh]:
     for nonlin in [torch.relu]:
         # for criterion in [F.cross_entropy, MSE_label]:
-        for criterion in [MSE_label]:
+        for criterion in [MSE_label if criterion_name == 'l2' else F.cross_entropy]:
 
-            for width in [64, 128, 256, 512, 1024, 2048, 4096, 8192]:
+            for width in [int(64 * width_mult)]:
                 # print(f'{nonlin.__name__}_{criterion.__name__}_{str(width)}')
-                mynet = MLP(width=width, nonlin=nonlin, output_mult=args.output_mult, input_mult=args.input_mult).to(device)
-                if args.save_base_shapes:
-                    print(f'saving base shapes at {args.save_base_shapes}')
+                mynet = MLP(width=width, nonlin=nonlin, output_mult=output_mult, input_mult=input_mult, growth_factor=growth_factor).to(device)
+                if save_base_shapes:
+                    print(f'saving base shapes at {save_base_shapes}')
                     base_shapes = get_shapes(mynet)
                     delta_shapes = get_shapes(
                         # just need to change whatever dimension(s) we are scaling
-                        MLP(width=width+1, nonlin=nonlin, output_mult=args.output_mult, input_mult=args.input_mult)
+                        MLP(width=width+1, nonlin=nonlin, output_mult=output_mult, input_mult=input_mult, growth_factor=growth_factor)
                     )
-                    make_base_shapes(base_shapes, delta_shapes, savefile=args.save_base_shapes)
+                    make_base_shapes(base_shapes, delta_shapes, savefile=save_base_shapes)
                     print('done and exit')
                     import sys; sys.exit()
-                if args.load_base_shapes:
-                    print(f'loading base shapes from {args.load_base_shapes}')
-                    set_base_shapes(mynet, args.load_base_shapes)
+                if load_base_shapes:
+                    print(f'loading base shapes from {load_base_shapes}')
+                    set_base_shapes(mynet, load_base_shapes)
                     print('done')
                 else:
                     print(f'using own shapes')
                     set_base_shapes(mynet, None)
                     print('done')
                 print('Width:', width)
-                optimizer = MuSGD(mynet.parameters(), lr=args.lr, momentum=args.momentum)
-                for epoch in range(1, args.epochs+1):
+                optimizer = MuSGD(mynet.parameters(), lr=lr, momentum=momentum)
+                for epoch in range(1, epochs+1):
                     train_loss, train_acc, = train(args, mynet, device, train_loader, optimizer, epoch, criterion=criterion)
-                    test_loss, test_acc = test(args, mynet, device, test_loader)
+                    if epoch - 1 > 0.8 * epochs:
+                        test_loss, test_acc = test(args, mynet, device, test_loader)
+                    else:
+                        test_loss, test_acc = None, None
                     logs.append(dict(
                         epoch=epoch,
                         train_loss=train_loss,
@@ -269,7 +280,7 @@ if __name__ == '__main__':
                     if math.isnan(train_loss):
                         break
     
-    with open(os.path.join(os.path.expanduser(args.log_dir), 'logs_{}.tsv'.format(args.run_index)), 'w') as f:
+    with open(os.path.join(os.path.expanduser(args.save_dir), 'logs.tsv'), 'w') as f:
         logdf = pd.DataFrame(logs)
-        print(os.path.join(os.path.expanduser(args.log_dir), 'logs_{}.tsv'.format(args.run_index)))
+        print(os.path.join(os.path.expanduser(args.save_dir), 'logs.tsv'))
         f.write(logdf.to_csv(sep='\t', float_format='%.4f'))
